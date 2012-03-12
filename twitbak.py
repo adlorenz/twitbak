1cb483bd7099983584a4a856aef31717b1fb01c1
Dawid Lorenz <adlorenz@gmail.com>
2012-03-12 10:05:33 +0000

    Work in progress - automatic mode not exactly working yet

--- twitbak.py
+++ twitbak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import json
 import optparse
+import os
 import sys
 import urllib2
 from dateutil.parser import parse
@@ -12,16 +13,19 @@ class Fetcher():
     
     BASE_URL = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=%s&include_rts=true&exclude_replies=%s'
     
+    exclude_replies = 'true'
+    since_id = None
+        
     def __init__(self, username, options):
         self.username = username
-        self.exclude_replies = 'true'
-        self.since_id = None
-        
         if options.include_replies:
             self.exclude_replies = 'false'
         
         if options.last_tweet_id:
             self.since_id = options.last_tweet_id
+            
+    def set_last_tweet_id(self, tweet_id):
+        self.since_id = tweet_id
             
     def get_url(self, page):
         url = self.BASE_URL % (self.username, self.exclude_replies)
@@ -38,23 +42,71 @@ class Fetcher():
         return raw_response
         
     
+class Config():
+    
+    last_tweet_id = None
+    
+    def __init__(self):
+        self.last_tweet_id_file = '%s/.twitbak.last_tweet_id' % os.environ.get('HOME')
+        self.last_tweet_id = self.get_last_tweet_id()
+    
+    def get_last_tweet_id(self):
+        if os.path.exists(self.last_tweet_id_file):
+            f = open(self.last_tweet_id_file, 'r')
+            last_tweet_id = f.read()
+            f.close()
+            return last_tweet_id.rstrip()
+        return None
+    
+    def set_last_tweet_id(self, tweet_id):
+        f = open(self.last_tweet_id_file, 'r')
+        f.write(tweet_id)
+        f.close()
+    
+    
 class Storage():
     """
     Manages tweets storage
     """
-    def __init__(self, output_path=None):
-        self.output_path = output_path or 'twitbak.txt'
-
-    def store(self, tweet):
+    final_path = 'tweets.txt'
+    tmp_path = None
+    writable_path = None
+    auto_mode = False
+    last_tweet_id_file = '.twitbak.last_tweet_id'
+    
+    def __init__(self, options):
+        if options.output_path:
+            self.final_path = options.output_path
+        self.tmp_path = '%s.tmp' % self.final_path
+        
+        if options.auto_mode:
+            self.auto_mode = True
+            self.writable_path = self.tmp_path
+        else:
+            self.writable_path = self.final_path
+            
+    def store_tweet(self, tweet):
         cols = [tweet.text, tweet.created_date, str(tweet.id)]
         line = "%s\n" % "\t".join(cols)
         line = line.encode('UTF-8')
         self.write(line)
         
-    def write(self, line):
-        file = open(self.output_path, 'a')
-        file.write(line)
-        file.close()
+    def write(self, content):
+        f = open(self.writable_path, 'a')
+        f.write(content)
+        f.close()
+        
+    def merge(self):
+        if self.auto_mode is False:
+            return
+        
+        if os.path.exists(self.final_path):
+            f = open(self.final_path, 'r')
+            self.write(f.read())
+            f.close()
+            os.remove(self.final_path)
+        os.rename(self.tmp_path, self.final_path)
+        
     
 class Parser():
     """
@@ -91,20 +143,30 @@ class Tweet():
     def created_date(self):
         return parse(self.data['created_at']).strftime('%Y-%m-%d %H:%M:%S')
     
-def spin(username, options):
+def spin(options, fetcher, storage, config):
     page = 1
+    #
+    # TODO: support auto mode properly
+    #
+    if options.auto_mode is True and config.last_tweet_id is not None:
+        sys.stdout.write('Running in AUTO mode, found last tweet ID %d' % config.last_tweet_id)
+        fetcher.set_last_tweet_id(config.last_tweet_id)
+        
     if options.page is not None:
         page = int(options.page)
     total_count = 0
+    retry_limit = 3
     prev_ids_buffer = []
-    fetcher = Fetcher(username, options)
-    storage = Storage(options.output_path)
     while (True):
         try:
             sys.stdout.write('Fetching page %d ' % page)
             tweets = Parser().parse_response(fetcher.fetch(page))
         except urllib2.HTTPError as e:
             sys.stderr.write("Oops: %s\n" % str(e))
+            retry_limit = retry_limit - 1
+            if retry_limit == 0:
+                sys.stderr.write("Giving up...\n")
+                break
             sys.stderr.write("Retrying...\n")
             continue
         
@@ -118,7 +180,7 @@ def spin(username, options):
         for tweet in tweets:
             current_ids_buffer.append(tweet.id)
             if tweet.id not in prev_ids_buffer:
-                storage.store(tweet)
+                storage.store_tweet(tweet)
                 current_count = current_count + 1
                 total_count = total_count + 1
             
@@ -126,35 +188,44 @@ def spin(username, options):
         prev_ids_buffer = current_ids_buffer
         sys.stdout.write('- processed %d tweets (%d total)\n' % (current_count, total_count))
 
+
 if __name__ == "__main__":
     parser = optparse.OptionParser("Usage: %prog [options] twitter_username")
+    parser.add_option('-a', '--auto', 
+                      action="store_true", 
+                      dest="automode", 
+                      help='Automatic mode - pick up last tweet ID from ~/.twitbak file and append new tweets at the beginning of output file')
     parser.add_option('-o', '--output-path', 
-                      action="store", 
-                      dest="output_path", 
+                      action="store_tweet", 
+                      dest="final_path", 
                       help='Path to the file where tweets should be saved')
-    parser.add_option('-p', '--page', 
-                      action="store", 
-                      dest="page", 
-                      help='Page number to start retrieving tweets from (by default start from page 1)')
     parser.add_option('-i', '--last-tweet-id', 
-                      action="store", 
+                      action="store_tweet", 
                       dest="last_tweet_id", 
-                      help='Only retrieve tweets newer than tweet with given ID')
+                      help='Manually specify tweet ID to only retrieve tweets newer than this')
+    parser.add_option('-p', '--page', 
+                      action="store_tweet", 
+                      dest="page", 
+                      help='Page number to start retrieving tweets from in Twitter\'s API (starts from page 1 by default). Useful to resume fetching after reaching hourly limit')
     parser.add_option('-r', '--include-replies', 
                       action="store_true", 
                       dest="include_replies", 
                       help='Should replies (tweets starting with @) be retrieved')
-    parser.add_option('-q', '--quiet', 
-                      action="store_true", 
-                      dest="quiet", 
-                      help='Do not produce any output')
+#    parser.add_option('-q', '--quiet', 
+#                      action="store_true", 
+#                      dest="quiet", 
+#                      help='Do not produce any output')
     
     (opts, args) = parser.parse_args()
     
     try:
         username = args[0]
-        spin(username, opts)
+        config = Config()
+        storage = Storage(opts)
+        fetcher = Fetcher(username, opts)
+        spin(opts, fetcher, storage, config)
+        storage.merge()
     except IndexError:
         sys.stderr.write('No twitter username specified\n')
     except KeyboardInterrupt:
-        pass
+        storage.merge()
